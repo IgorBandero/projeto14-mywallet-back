cdb2753e6ed4114cbc524338326f5db08869d014
Igor Bandero <igorbandero@gmail.com>
2023-07-11 04:33:39 -0300

    Requisitos gerais implementados

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,10 @@
+import {ObjectId} from "mongodb";
+import {db} from "../database/dbConection.js";
+
+export async function createUser(req, res) {
+
+}
+
+export async function getUser(req, res) {
+
+}
---
+++ src/middlewares/validateUser.js
@@ -0,0 +1,5 @@
+import {db} from "../database/dbConnection.js";
+
+export async function validateLogin(req, res, next) {
+    
+}
